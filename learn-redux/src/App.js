021ed9cfbdc1e5dc0e9ca556cbf4c0381cce9d35
kynel <kynel95@gmail.com>
2021-09-02 17:53:45 +0900

    implement Todos with Redux

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -1,9 +1,12 @@
 import CounterContainer from "./components/Containers/CounterContainer";
+import TodosContainer from "./components/Containers/TodoContainer";
 
 function App() {
   return (
     <>
       <CounterContainer />
+      <hr />
+      <TodosContainer />
     </>
   );
 }
