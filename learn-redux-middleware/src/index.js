f84fc790ea1de79b2ec946a4235be0d351456bf9
kynel <kynel95@gmail.com>
2021-09-15 15:34:36 +0900

    install redux-logger and redux-devtools
    install redux-logger, redux-devtools-extension and apply in store

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -5,9 +5,13 @@ import reportWebVitals from "./reportWebVitals";
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./modules";
-import myLogger from "./middlewares/myLogger";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 ReactDOM.render(
   <React.StrictMode>
